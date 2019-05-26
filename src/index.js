d00179702bffe987a279478b80ea3d71a64efbde
Jon Huh <jonhuh@Jons-MacBook-Pro.local>
2019-05-26 21:01:36 +0900

    test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import request from 'request';
 import http from 'http';
 import socket from 'socket.io';
 
+import util from 'util';
+import { exec } from 'child_process'
+
+const execPromise = util.promisify(exec);
+
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
@@ -18,6 +23,46 @@ app.use(bodyParser.json());
 app.get('/ping', (req, res) => {
   res.send('Hello World');
 });
+
+app.post('/deployment/fib-calculator', async (req, res) => {
+  try {
+    const { stdout, stderr } = await execPromise('kubectl apply -f /home/sysadm/workspace/fib-calculator-k8s/k8s');
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+
+      return res.status(422).send({ status: 'ERROR', message: `stderr: ${stderr}` })
+    }
+    console.log(`stdout: ${stdout}`);
+
+    return res.send({ status: 'OK', message: `stdout: ${stdout}` })
+
+  } catch (err) {
+    console.log(err)
+
+    return res.status(422).send({ status: 'ERROR', message: `err: ${err}` })
+  }
+})
+
+app.delete('/deployment/fib-calculator', async (req, res) => {
+  try {
+    const { stdout, stderr } = await execPromise('kubectl delete -f /home/sysadm/workspace/fib-calculator-k8s/k8s');
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+
+      return res.status(422).send({ status: 'ERROR', message: `stderr: ${stderr}` })
+    }
+    console.log(`stdout: ${stdout}`);
+
+    return res.send({ status: 'OK', message: `stdout: ${stdout}` })
+
+  } catch (err) {
+    console.log(err)
+
+    return res.status(422).send({ status: 'ERROR', message: `err: ${err}` })
+  }
+})
 
 request('http://127.0.0.1:8001/api/v1/watch/namespaces/default/pods/').pipe(process.stdout)
 

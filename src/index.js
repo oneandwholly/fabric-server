5abdf5badf1c7b10b893eae1a5855170e8287aa4
Jon Huh <jonhuh@Jons-MacBook-Pro.local>
2019-05-26 21:25:28 +0900

    hmm

--- src/index.js
+++ src/index.js
@@ -75,8 +75,6 @@ var writable = new stream.Writable({
 
 request('http://127.0.0.1:8001/api/v1/watch/namespaces/default/pods/').pipe(writable)
 
-tryConnectingToPodStatus
-
 io.on('connection', client => {
   client.on('event', data => { /* … */ });
   client.on('disconnect', () => { /* … */ });

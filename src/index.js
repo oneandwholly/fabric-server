656c942610f7f7a16ff4cddd4c0978b0345febea
Jon Huh <jonhuh@Jons-MacBook-Pro.local>
2019-05-26 20:27:46 +0900

    test io

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-import request from 'request'
+import request from 'request';
+import http from 'http';
+import socket from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -17,6 +21,13 @@ app.get('/ping', (req, res) => {
 
 request('http://127.0.0.1:8001/api/v1/watch/namespaces/default/pods/').pipe(process.stdout)
 
-export default app.listen(5000, err => {
+io.on('connection', client => {
+  client.on('event', data => { /* … */ });
+  client.on('disconnect', () => { /* … */ });
+});
+
+server.listen(5000, err => {
   console.log('Listening');
 });
+
+export default server
